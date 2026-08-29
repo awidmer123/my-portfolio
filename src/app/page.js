0b2e0273c60e9fce0b318cc8ebec503d4fee6486
awidmer123 <andrileviwidmer@gmail.com>
2026-08-29 19:24:54 +0200

    another sentence in the about-section

--- src/app/page.js
+++ src/app/page.js
@@ -34,8 +34,8 @@ export default async function Home() {
         <div className="space-y-4 text-gray-700 leading-relaxed max-w-2xl">
           <p>
             I'm a bioinformatician and computational biology student based in Switzerland that pretends to have a hobby to keep up his image of not being too nerdy. Photography is one of those things I enjoy, yet somehow never do enough of, since I am, as all of my generation, addicted to screens and basically got no time to do anything else than doomscrolling. I also feel a need to share something with the world that is less unsettling than the current political climate or the various crises we face. I didn't like the idea of an instagram page, so there you go. Here you can find my best, or alternatively least bad, shots so far. I hope you find something nice and aesthetic.
-            With this project I'm hoping to change that, giving myself a reason to pick up the camera more regularly.
-            A little self-imposed push, if you will (Eeeeew that sounds almost like one of those hypocritical alpha-male-life-coaches).
+            With this project I'm hoping to give myself a reason to pick up the camera more regularly.
+            A little self-imposed push, if you will (Eeeeew that sounds almost like one of those hypocritical alpha-male-life-coaches (pay attention, these are bad people)).
           </p>
           <p>
             No professional here. My photos are unedited and straight from the camera, which is exactly how I want it (I am a lazy f*** who does neither have the money for proper software nor the skills to edit photos). I love being outside and capturing moments consciously ("consciously": Just hoping to get the settings right enough on an analog camera with a broken photosensor (Konica AF) to see at least something. For sure with the other cameras it is a bit less of a hassle.).

00381105136ada3249ee097405b4fe2d4376c35c
awidmer123 <andrileviwidmer@gmail.com>
2026-08-30 10:39:06 +0200

    new sidebar text

--- src/app/components/Sidebar.js
+++ src/app/components/Sidebar.js
@@ -32,7 +32,7 @@ export default function Sidebar({ albums }) {
         lg:translate-x-0
       `}>
         <div className="mb-8">
-          <h1 className="text-xl font-bold">📷 My Portfolio</h1>
+          <h1 className="text-xl font-bold">Menu</h1>
         </div>
 
         <nav className="space-y-2">
